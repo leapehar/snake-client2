fb19b8fe501fd136fbe419d0a60a6386f21c945b
Lea Pehar <leapehar@gmail.com>
2022-01-20 02:14:58 +0000

    moved connect function into client js file. Required connect funtion into play js

--- play.js
+++ play.js
@@ -1,24 +1,25 @@
-const net = require("net");
+const {connect} = require("./client.js");
+// const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
+// // establishes a connection with the game server
+// const connect = function() {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: 50541,
+//   });
 
-  //Updating the connect function to handle incoming data and console.log it for the player.
-  conn.on('data', (data) => {
-    console.log('Message to player: ', data)
-  });
+//   //Updating the connect function to handle incoming data and console.log it for the player.
+//   conn.on('data', (data) => {
+//     console.log('Message to player: ', data)
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
 
 
